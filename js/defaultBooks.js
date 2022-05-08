87fe62b79009881a7aaf6cc71e5afc9db6e549e5
Travis <travis.m.bruce@gmail.com>
2022-05-07 22:30:27 -0700

    Modify header for sticky positioning

--- js/defaultBooks.js
+++ js/defaultBooks.js
@@ -39,6 +39,526 @@ let defualtBooks = [
     imgSrc:
       "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
   },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
+  {
+    title: "This Is Your Mind on Plants",
+    author: "Michael Pollan",
+    isbn: "0593296907",
+    year: "2021",
+    pages: "288",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=Fxs3EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    isbn: "0132350882",
+    year: "2009",
+    pages: "431",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "How To Change Your Mind",
+    author: "Michael Pollan",
+    isbn: "0525558942",
+    year: "2018",
+    pages: "480",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=3vk5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+  },
+  {
+    title: "Cracking the Coding Interview",
+    author: "Gayle Laakmann McDowell",
+    isbn: "0984782850",
+    year: "2015",
+    pages: "708",
+    read: "true",
+    imgSrc:
+      "http://books.google.com/books/content?id=jD8iswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  },
 ];
 
 export default defualtBooks;
